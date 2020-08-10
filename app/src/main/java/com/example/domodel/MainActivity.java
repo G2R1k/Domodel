9b7bdd20c8ae7c94d634f4684066c25a0d5e75b3
G2R1k <eerror123@yandex.ru>
2020-08-10 15:40:25 +0300

    Start InfoActivity

--- app/src/main/java/com/example/domodel/MainActivity.java
+++ app/src/main/java/com/example/domodel/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.domodel;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private String LOGIN = "Dima";
+    final private String PASSWORD = "Ivanov";
+
+    EditText textLogin;
+    EditText textPassword;
+    Button btnEnter;
+
+    View.OnClickListener enterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(checkLoginPassword()){
+                Intent startInfoActivity = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(startInfoActivity);
+                showMsg(R.string.accessLogin);
+            } else {
+                showMsg(R.string.errorLogin);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        textLogin = findViewById(R.id.etLogin2);
+        textPassword = findViewById(R.id.etPassword2);
+        btnEnter = findViewById(R.id.btnEnter);
+
+        btnEnter.setOnClickListener(enterClickListener);
+    }
+
+    private boolean checkLoginPassword(){
+        return textLogin.getText().toString().equals(LOGIN) && textPassword.getText().toString().equals(PASSWORD);
+    }
+
+    private void showMsg(@StringRes int str){
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
     }
 }
